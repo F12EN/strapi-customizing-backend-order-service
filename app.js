b03838e659a65e1c826e9c07796fa2c0c9d55eb5
miracleonyenma <miracleficient@gmail.com>
2022-05-16 09:48:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Koa = require("koa");
+const logger = require("koa-logger")
+
+const app = new Koa();
+app.use(logger())
+
+app.use(async (ctx) => {
+  console.log(ctx.request)
+  ctx.body = "ok"
+});
+
+app.listen(3000, () => {
+  console.log("Order service running")
+})
